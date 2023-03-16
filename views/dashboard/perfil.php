5cbe8183f88a1038a60c88cb87ed5c13f4711ac8
Victor Oss <vicjosneros@gmail.com>
2023-03-15 18:11:20 -0600

    Agregado: menu responsivo , script para el menu en vista mobile

--- views/dashboard/perfil.php
+++ views/dashboard/perfil.php
@@ -1,4 +1,6 @@
 <?php include_once __DIR__."/headerDashboard.php"; ?>
 
-
+<div class="contenedor-sm">
+    
+</div>
 <?php include_once __DIR__."/footerDashboard.php"; ?>
--- views/templates/barra.php
+++ views/templates/barra.php
@@ -1,3 +1,9 @@
+<div class="barra-mobile">
+    <h1>UpTask</h1>
+    <div class="menu">
+        <img src="/build/img/menu.svg" id="mobile-menu" alt="img menu">
+    </div>
+</div>
 <div class="barra">
     <p>Hola: <span> <?php  echo $_SESSION["nombre"]; ?></span></p>
     <a href="/logout" class="cerrar-sesion">Cerrar Sesión</a>
